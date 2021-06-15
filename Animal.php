3b4eeeaf464569ba49d622ff3e71dcd690d95be4
kosakuka <playlikeharden@gmail.com>
2021-06-15 17:46:05 +0900

    プログラムで使用するクラスのプログラムを作成

---
+++ Animal.php
@@ -0,0 +1,27 @@
+<?php
+class Animal{
+    public function __construct(){
+        $this->iv = rand(0,4);
+        //その動物の個体値（Individual Value）要するに才能　例えば、同じ猫の中でも
+        //強い弱いが存在するわけだが、それを5段階の値で表している
+        $this->strength = 0;//動物の最終的な攻撃力　とりあえず0で初期化（継承先で設定）
+        $this->voice = '';//その動物の鳴き声　とりあえず空の文字列で初期化
+        $this->name = '';//その動物の名前
+    }
+    public function eat($anml){
+        echo "こちらの{$this->name}の攻撃力は{$this->strength}";
+        echo "あいての{$anml->name}の攻撃力は{$anml->strength}";
+        if($this->strength > $anml->strength){
+            echo "相手を捕食した";
+            $anml->__destruct();
+        }elseif($this->strength == $anml->strength){
+            echo "相打ちになった";
+        }else{
+            echo "返り討ちにあった";
+            $this->__destruct();
+        }
+    }
+    public function __destruct(){
+        echo "{$this->voice}...";//食べられる際の鳴き声
+    }
+}
---
+++ Lion.php
@@ -0,0 +1,12 @@
+<?php
+
+require('./Animal.php');
+class Lion extends Animal{
+    public function __construct(){
+        parent::__construct();
+        $this->strength = 10 + $this->iv;
+        $this->name = "らいおん";
+        $this->voice = "gaooo";
+    }
+    
+}
