8b96cb86527b02e75e0f997f8a049f36d5160012
kosakuka <playlikeharden@gmail.com>
2021-06-15 18:39:00 +0900

    らいおんとひらがな表記になってたため、カタカナに修正

--- Lion.php
+++ Lion.php
@@ -5,7 +5,7 @@ class Lion extends Animal{
     public function __construct(){
         parent::__construct();
         $this->strength = 10 + $this->iv;
-        $this->name = "らいおん";
+        $this->name = "ライオン";
         $this->voice = "gaooo";
     }
     
